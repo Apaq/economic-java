41f105132910586693049c31cbd96c7601a115e0
Michael Krog <mic@apaq.dk>
2020-01-07 07:39:13 +0100

    Changes resource

--- src/main/java/dk/previsto/economic/repository/Resource.java
+++ src/main/java/dk/previsto/economic/repository/Resource.java
@@ -62,8 +62,8 @@ public abstract class Resource<T extends Persistable<String>> {
 
         UriComponentsBuilder builder = UriComponentsBuilder.fromUri(url);
         if (pageRequest != null) {
-            builder.queryParam("page", pageRequest.getPageNumber() + 1);
-            builder.queryParam("pageSize", pageRequest.getPageSize());
+            builder.queryParam("skippages", pageRequest.getPageNumber());
+            builder.queryParam("pagesize", pageRequest.getPageSize());
         }
         
         for (Map.Entry<String, String> entry : sideloadParams.entrySet()) {
