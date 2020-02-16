d307e412b29f8af4ae964153491efc413cac30d2
Michael Krog <mic@apaq.dk>
2020-02-16 22:10:01 +0100

    Adds support for downloading PDF.

--- src/main/java/dk/previsto/economic/repository/BookedInvoiceResource.java
+++ src/main/java/dk/previsto/economic/repository/BookedInvoiceResource.java
@@ -1,14 +1,19 @@
 package dk.previsto.economic.repository;
 
+import dk.previsto.economic.exception.ApiException;
 import dk.previsto.economic.exception.RequestException;
+import dk.previsto.economic.exception.ResourceNotFoundException;
 import dk.previsto.economic.model.BookInvoiceRequest;
 import dk.previsto.economic.model.BookedInvoice;
 import dk.previsto.economic.model.DraftInvoice;
 import dk.previsto.economic.model.references.DraftInvoiceReference;
 import org.springframework.data.domain.Persistable;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 
 public class BookedInvoiceResource extends Resource<BookedInvoice>{
@@ -35,5 +40,20 @@ public class BookedInvoiceResource extends Resource<BookedInvoice>{
         persistedEntity = response.getBody();
 
         return persistedEntity;
+    }
+
+    public InputStream getPdf(String id) {
+        BookedInvoice bookedInvoice = get(id);
+        if(bookedInvoice == null) {
+            throw new ResourceNotFoundException("404", "Resource does not exist.", "");
+        }
+        ResponseEntity<org.springframework.core.io.Resource> responseEntity = restTemplate.exchange( bookedInvoice.getPdf().getDownload(), HttpMethod.GET, null, org.springframework.core.io.Resource.class );
+
+        try {
+            return responseEntity.getBody().getInputStream();
+        }
+        catch (IOException e) {
+            throw new ApiException("", "Exception occured while downloading PDF.", "");
+        }
     }
 }
